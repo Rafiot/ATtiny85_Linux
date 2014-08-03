86d635b1f6cc739b1761c72d962d55f80cf30825
Raphaël Vinot <raphael@vinot.info>
2014-08-03 15:26:39 +0200

    add example ring arduino

---
+++ example_ring/README.md
@@ -0,0 +1,17 @@
+Needed to build
+===============
+
+Pixel library:
+
+```
+mkdir -p ~/sketchbook/libraries
+cd ~/sketchbook/libraries
+git clone git@github.com:adafruit/Adafruit_NeoPixel.git
+```
+
+Arduino-Makfile project:
+
+```
+cd ~/gits
+git clone https://github.com/sudar/Arduino-Makefile.git
+```
--- fetch.sh
+++ fetch.sh
@@ -5,7 +5,10 @@ wget http://learn.adafruit.com/system/assets/assets/000/010/777/original/trinket
 
 unzip trinkethardwaresupport.zip
 
-cp hardware/attiny/variants/tiny8/pins_arduino.h example/
+cp hardware/attiny/variants/tiny8/pins_arduino.h .
+
+cp pins_arduino.h example_ring/
+cp pins_arduino.h example_blink/
 
 rm -rf hardware trinkethardwaresupport.zip
 
