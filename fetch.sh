e3ba33e802249030877e58bfd0eb37e7d74daf1b
Steve Clement <steve@localhost.lu>
2015-01-11 13:03:48 +0100

    - cp regression

--- fetch.sh
+++ fetch.sh
@@ -7,7 +7,7 @@ unzip trinkethardwaresupport.zip
 
 cp hardware/attiny/variants/tiny8/pins_arduino.h .
 
-mv pins_arduino.h example_ring/
+cp pins_arduino.h example_ring/
 mv pins_arduino.h example_blink/
 
 rm -rf hardware trinkethardwaresupport.zip
