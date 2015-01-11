e9890b4a175df4d8d6a8a91b8be67c0a49f26d94
Steve Clement <steve@localhost.lu>
2015-01-11 12:55:54 +0100

    - Added Arduino-Makefile and Adafruit_NeoPixel as submodules, update trinkethardware download location

---
+++ README.md
@@ -0,0 +1,3 @@
+Easy-install for the Adafruit Gemma Miniature Wearable Electronic Platform (Arduino compatible)
+
+https://www.adafruit.com/product/1222
--- fetch.sh
+++ fetch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-wget http://learn.adafruit.com/system/assets/assets/000/010/777/original/trinkethardwaresupport.zip?1378321062 -O trinkethardwaresupport.zip
+wget http://www.adafruit.com/downloads/trinkethardwaresupport.zip
 
 unzip trinkethardwaresupport.zip
 
 cp hardware/attiny/variants/tiny8/pins_arduino.h .
 
-cp pins_arduino.h example_ring/
-cp pins_arduino.h example_blink/
+mv pins_arduino.h example_ring/
+mv pins_arduino.h example_blink/
 
 rm -rf hardware trinkethardwaresupport.zip
 
